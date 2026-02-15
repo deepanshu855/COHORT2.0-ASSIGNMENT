50e76895dbef9bb512e81dfdf4491778e5f25ed8
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-02-15 08:21:33 +0530

    Added cursor

--- ASSIGNMENT-14/script.js
+++ ASSIGNMENT-14/script.js
@@ -26,15 +26,23 @@ body.addEventListener("keyup", function (elem) {
 
 keys.forEach(function (key) {
   key.addEventListener("click", function (elem) {
-    let note= key.dataset.note;
-    
-    let audio= document.querySelector(`audio[data-note="${note}"]`);
-    audio.currentTime= 0;
+    let note = key.dataset.note;
+
+    let audio = document.querySelector(`audio[data-note="${note}"]`);
+    audio.currentTime = 0;
     audio.play();
 
     key.classList.add("active");
-    setTimeout(function(){
-        key.classList.remove("active");
-    }, 100)
+    setTimeout(function () {
+      key.classList.remove("active");
+    }, 100);
   });
+});
+
+let cursor = document.querySelector("#cursor");
+
+document.body.addEventListener("mousemove", function (elem) {
+  cursor.style.top = elem.clientY + "px";
+  cursor.style.left = elem.clientX + "px";
+  console.log(elem.clientX, elem.clientY)
 });
